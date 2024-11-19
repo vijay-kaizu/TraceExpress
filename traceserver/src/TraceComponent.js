b96f3ae8f67a40dc63716a8c444ca56e3ed7974a
vijay-kaizu <vmaramreddy@kiku-km.com>
2024-11-19 18:23:47 +0530

    RED-18 - Disable search execution by hitting enter key. (Search is only executed by hitting Search icon.

--- traceserver/src/TraceComponent.js
+++ traceserver/src/TraceComponent.js
@@ -99,112 +99,114 @@ const TraceComponent = () => {
             ) : !listLoaded ? (
                 <div>{t('Loading...')}</div>
             ) : (
-                <Table>
+                <Table bordered={false}>
                     <tbody>
-                    <tr>
-                        <td style={searchControlsStyle}>
-                            <form onSubmit={handleSubmit}>
-                                <div className="form-group">
-                                    <label htmlFor="company">{"company"}</label>
-                                    <input type="text" value={searchCompany}
-                                           onChange={(e) => {
-                                               sessionStorage.setItem("searchCompany", e.target.value)
-                                               setSearchCompany(e.target.value)
-                                           }}
-                                           className="form-control form-control-sm" id="company"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="warehouse">{t('warehouse')}</label>
-                                    <input type="text" value={searchWarehouse}
-                                           onChange={(e) => {
-                                               sessionStorage.setItem('searchWarehouse', e.target.value)
-                                               setSearchWarehouse(e.target.value)
-                                           }}
-                                           className="form-control form-control-sm" id="warehouse"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="product">{t('product')}</label>
-                                    <input type="text" value={searchProduct}
-                                           onChange={(e) => {
-                                               sessionStorage.setItem("searchProduct", e.target.value)
-                                               setSearchProduct(e.target.value)
-                                           }}
-                                           className="form-control form-control-sm" id="product"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="lot">{t('lot')}</label>
-                                    <input type="text" value={searchLot}
-                                           onChange={(e) => {
-                                               sessionStorage.setItem('searchLot', e.target.value)
-                                               setSearchLot(e.target.value)
-                                           }}
-                                           className="form-control form-control-sm" id="lot"/>
-                                </div>
-                                <Button type="submit">{t('search')}</Button>
-                            </form>
-                        </td>
-                        <td>
-                            {!isFirstLoad ?
-                                <Table striped bordered hover size="sm">
-                                    <thead>
-                                    <tr>
-                                        <td>{t('company')}</td>
-                                        <td>{t('warehouse')}</td>
-                                        <td>{t('product')}</td>
-                                        <td>{t('lot')}</td>
-                                        <td>{t('status')}</td>
-                                        <td>{t('sequence')}</td>
-                                        <td>{t('UOM')}</td>
-                                        <td>{t('UOM_type')}</td>
-                                        <td>{t('quantity')}</td>
-                                        <td>{t('quantity_picked')}</td>
-                                        <td>{t('quantity_in_despatch')}</td>
-                                        <td>{t('quantity_reserved')}</td>
-                                        <td>{t('quantity_awaiting_confirm_out')}</td>
-                                        <td>{t('quantity_available')}</td>
-                                        <td>{t('gem_archive_flag')}</td>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    {listItems.length > 0 ? (
-                                        listItems.map(item => (
-                                            <tr key={item.id}>
-                                                <td>{item.COMPANY_CODE}</td>
-                                                <td>{item.WAREHOUSE}</td>
-                                                <td>{item.PART_CODE}</td>
-                                                <td>{item.IC_LOT_NUMBER}</td>
-                                                <td>{item.IC_STATUS_CODE}</td>
-                                                <td>{item.IC_LOT_SEQUENCE}</td>
-                                                <td>{item.UNIT_OF_MEASURE}</td>
-                                                <td>{item.IC_PRODUCT_UOM_TYPE}</td>
-                                                <td>{item.IC_QUANTITY}</td>
-                                                <td>{item.IC_QTY_PICKED}</td>
-                                                <td>{item.IC_QTY_IN_DESPATCH}</td>
-                                                <td>{item.IC_QTY_RESERVED}</td>
-                                                <td>{item.IC_QTY_AWAITING_CONFIRM_OUT}</td>
-                                                <td>{item.IC_QTY_AVAILABLE}</td>
-                                                <td>{item.GEM_ARCHIVE_FLAG}</td>
-                                                <td>
-                                                    <IconButton aria-label="view" size="small"
-                                                                href={`/item/${item.WAREHOUSE}/${item.PART_CODE}/${item.IC_LOT_NUMBER}`}>
-                                                        <ArrowForwardIosIcon fontSize="inherit"/>
-                                                    </IconButton>
-                                                </td>
-                                            </tr>
-                                        ))
-                                    ) : (
-                                        <tr>
-                                            <td colSpan="5">{isFirstLoad ? '' : t('no_matching_records')}</td>
+                    <td style={searchControlsStyle}>
+                        <form onSubmit={handleSubmit}
+                              onKeyDown={(e) => {
+                                  if (e.key === "Enter") {
+                                      e.preventDefault();
+                                  }
+                              }}>
+                            <div className="form-group">
+                                <label htmlFor="company" className="form-label">{t('company')}</label>
+                                <input type="text" value={searchCompany}
+                                       onChange={(e) => {
+                                           sessionStorage.setItem("searchCompany", e.target.value)
+                                           setSearchCompany(e.target.value)
+                                       }}
+                                       className="form-control form-control-sm" id="company"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="warehouse" className="form-label">{t('warehouse')}</label>
+                                <input type="text" value={searchWarehouse}
+                                       onChange={(e) => {
+                                           sessionStorage.setItem('searchWarehouse', e.target.value)
+                                           setSearchWarehouse(e.target.value)
+                                       }}
+                                       className="form-control form-control-sm" id="warehouse"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="product" className="form-label">{t('product')}</label>
+                                <input type="text" value={searchProduct}
+                                       onChange={(e) => {
+                                           sessionStorage.setItem("searchProduct", e.target.value)
+                                           setSearchProduct(e.target.value)
+                                       }}
+                                       className="form-control form-control-sm" id="product"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="lot" className="form-label">{t('lot')}</label>
+                                <input type="text" value={searchLot}
+                                       onChange={(e) => {
+                                           sessionStorage.setItem('searchLot', e.target.value)
+                                           setSearchLot(e.target.value)
+                                       }}
+                                       className="form-control form-control-sm" id="lot"/>
+                            </div>
+                            <Button type="submit">{t('search')}</Button>
+                        </form>
+                    </td>
+                    <td>
+                        {!isFirstLoad ?
+                            <Table striped bordered hover size="sm">
+                                <thead>
+                                <tr>
+                                    <td>{t('company')}</td>
+                                    <td>{t('warehouse')}</td>
+                                    <td>{t('product')}</td>
+                                    <td>{t('lot')}</td>
+                                    <td>{t('status')}</td>
+                                    <td>{t('sequence')}</td>
+                                    <td>{t('UOM')}</td>
+                                    <td>{t('UOM_type')}</td>
+                                    <td>{t('quantity')}</td>
+                                    <td>{t('quantity_picked')}</td>
+                                    <td>{t('quantity_in_despatch')}</td>
+                                    <td>{t('quantity_reserved')}</td>
+                                    <td>{t('quantity_awaiting_confirm_out')}</td>
+                                    <td>{t('quantity_available')}</td>
+                                    <td>{t('gem_archive_flag')}</td>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {listItems.length > 0 ? (
+                                    listItems.map(item => (
+                                        <tr key={item.id}>
+                                            <td>{item.COMPANY_CODE}</td>
+                                            <td>{item.WAREHOUSE}</td>
+                                            <td>{item.PART_CODE}</td>
+                                            <td>{item.IC_LOT_NUMBER}</td>
+                                            <td>{item.IC_STATUS_CODE}</td>
+                                            <td>{item.IC_LOT_SEQUENCE}</td>
+                                            <td>{item.UNIT_OF_MEASURE}</td>
+                                            <td>{item.IC_PRODUCT_UOM_TYPE}</td>
+                                            <td>{item.IC_QUANTITY}</td>
+                                            <td>{item.IC_QTY_PICKED}</td>
+                                            <td>{item.IC_QTY_IN_DESPATCH}</td>
+                                            <td>{item.IC_QTY_RESERVED}</td>
+                                            <td>{item.IC_QTY_AWAITING_CONFIRM_OUT}</td>
+                                            <td>{item.IC_QTY_AVAILABLE}</td>
+                                            <td>{item.GEM_ARCHIVE_FLAG}</td>
+                                            <td>
+                                                <IconButton aria-label="view" size="small"
+                                                            href={`/item/${item.WAREHOUSE}/${item.PART_CODE}/${item.IC_LOT_NUMBER}`}>
+                                                    <ArrowForwardIosIcon fontSize="inherit"/>
+                                                </IconButton>
+                                            </td>
                                         </tr>
-                                    )}
-                                    </tbody>
-                                </Table> : null}
-                        </td>
-                    </tr>
+                                    ))
+                                ) : (
+                                    <tr>
+                                        <td colSpan="5">{isFirstLoad ? '' : t('no_matching_records')}</td>
+                                    </tr>
+                                )}
+                                </tbody>
+                            </Table> : null}
+                    </td>
                     </tbody>
                 </Table>
-            )
-            }
+            )}
         </div>);
 };
 
